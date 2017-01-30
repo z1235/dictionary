1157a59eef5671d776f93764b295bb30c6351774
hui <799488441@qq.com>
2017-01-30 20:49:09 +0800

    基于binary search tree的词典

---
+++ BST/bst.h
@@ -0,0 +1,31 @@
+#ifndef BST_H
+#define BST_H
+
+#include <string>
+using namespace std;
+
+typedef struct Position{
+	unsigned int line;
+	unsigned int offset;
+	Position *next;
+}Position;
+
+typedef struct Node{
+	string word;
+	Position pos;
+	struct Node *lchild;
+	struct Node *rchild;	
+	struct Node *parent;
+}Node, *Bstree;
+
+void initBST(Bstree &T);
+void insertBST(Bstree &T, string word, unsigned int line, unsigned int offset);
+void inOrderBST(Bstree &T);/*中序遍历binary search tree*/
+Node * searchBST(Bstree &T, string word);
+Node * createNode(string word, unsigned int line, unsigned int offset);
+void addPosition(Node *pNode, unsigned int line, unsigned int offset);
+Node * searchBST(Bstree &T, string word);
+void display_result(Node *node);
+
+
+#endif
